4091ad0332b9390bc5954a5278466233da9e6944
leonardoromero <leonardorommero@gmail.com>
2022-11-10 13:30:33 -0300

    Fix contact section

--- pages/sections/Contact/Contact.tsx
+++ pages/sections/Contact/Contact.tsx
@@ -1,17 +1,14 @@
 import Script from 'next/script'
 
-const Contact = () => {
-	return (
-		<div className="contact-container" id='contact'>
-				<iframe
-					width="100%"
-					height="100%"
-					frameborder="0"
-					allowtransparency="true"
-					src="https://www.surveymonkey.com/r/FYKP5GX?embedded=1"
-				></iframe>
-		</div>
-	)
-}
+const Contact = () => (
+	<div className="contact-container" id="contact">
+		<iframe
+			frame-title="formulario de contacto"
+			width="100%"
+			height="100%"
+			src="https://www.surveymonkey.com/r/FYKP5GX?embedded=1"
+		></iframe>
+	</div>
+)
 
 export default Contact
